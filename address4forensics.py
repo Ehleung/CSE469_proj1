ae6ae6c65a8f269c5241f50130e378b8e461868c
Ellery Leung <Ellery.h.leung@gmail.com>
2018-04-04 13:04:30 -0700

    Removed main func

--- address4forensics.py
+++ address4forensics.py
@@ -1,13 +1,6 @@
-
 import argparse
 
-def main():
-
-	parser = argparse.ArgumentParser()
-	parser.add_argument( '-L', '--logical',
-					help="Calculate the logical address from either the cluster address" +
-					 " or the physical address. Either –c or –p must be given."
-						)
-	parser.parse_args()
-
-main()
+parser = argparse.ArgumentParser()
+parser.add_argument('-L', '--logical', metavar='', help='Calculate the logical address from either the cluster ' +
+											'address or the physical address. Either -c or -p must be given.')
+results = parser.parse_args()
