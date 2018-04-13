01e5e17756f53dfa2b45643294da5bfdc07ab681
Ehleung <Ellery.h.leung@gmail.com>
2018-04-13 11:37:08 -0700

    file cleanup cuz brandon sucks
    Co-Authored-By: bnydam <bnydam@users.noreply.github.com>

--- mac_conversion.py
+++ mac_conversion.py
@@ -1,10 +1,10 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+DEBUG = True
 
-# -T|-D [–f filename | -h hex value ]
+parser = argparse.ArgumentParser(add_help = False)
 
-conv_group = parser.add_mutually_exclusive_group(required=True)
+conv_group = parser.add_mutually_exclusive_group(required = True)
 conv_group.add_argument('-T', action='store_true',
 						help='Set this flag to perform a time conversion. ' +
 						'Either \'-f\' or \'-h\' must be provided.')
@@ -12,7 +12,24 @@ conv_group.add_argument('-D', action='store_true',
 						help='Set this flag to perform a date conversion. ' +
 						'Either \'-f\' or \'-h\' must be provided.')
 
-param_group = parser.add_mutually_exclusive_group(required=True)
+param_group = parser.add_mutually_exclusive_group(required = True)
 
-parser.add_argument('-f', dest='filename',
-					help='Specifies the path to a filename that includes a hex value')
+
+param_group.add_argument('-f', dest='filename', help='Specifies the path to a filename that includes a hex value')
+param_group.add_argument('-h', dest='hexval', help='Specifies the Hex value to convert')
+
+args = parser.parse_args()
+if DEBUG:
+	print (args)
+
+
+if args.T:
+	print('T')
+	file = open(args.filename, 'r')
+	var = file.readline()
+	file.close()
+else:
+    var = args.hexval
+    
+    
+    print('D')
