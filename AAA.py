db7fdd2956def66cf25b2edc639743d6ff81fa21
Bnydam <bnydam@asu.edu>
2018-04-20 11:20:51 -0700

    Update AAA.py
    Need to edit parsing of pathname for filename

--- AAA.py
+++ AAA.py
@@ -1,14 +1,15 @@
 import hashlib
+import sys
 
 DEBUG = True
 
 def hash_checksum(filename):
     if DEBUG:
-        print 'Generating Checksums'
-        print "sha1-{}.txt".format(filename)
+        print ('Generating Checksums')
+        #print ("sha1-{}.txt".format(filename))
     md5file = open("md5-{}.txt".format(filename), 'w+')
     shafile = open("sha1-{}.txt".format(filename), 'w+')
-
+    
     md5file.write(hashlib.md5(open(filename, 'rb').read()).hexdigest())
     shafile.write(hashlib.sha1(open(filename, 'rb').read()).hexdigest())    
     
@@ -16,8 +17,17 @@ def hash_checksum(filename):
     shafile.close()
     
     if DEBUG:
-        print 'Checksums Generated'
+        print ('Checksums Generated')
 
 if __name__ == "__main__":
-    hash_checksum('testFile.txt')
-    
+    #print(sys.argv[1])
+
+    try:
+        open(sys.argv[1], 'r')
+    except OSError:
+        print ('Error, File not found')
+        exit
+
+    print(sys.argv)
+    hash_checksum(sys.argv[1])
+    print('exiting')
