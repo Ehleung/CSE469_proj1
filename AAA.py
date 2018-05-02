ae7af4e1214bd5e546b3fa43969d2c34dc2df085
Bnydam <bnydam@asu.edu>
2018-05-01 17:47:05 -0700

    Add files via upload

--- AAA.py
+++ AAA.py
@@ -1,6 +1,13 @@
+'''
+Brandon Nydam 1208178552
+Ellery Leung 1207157168
+CSE469 Project 1
+'''
+
+
 import hashlib
 import sys
-import binascii
+
 
 DEBUG = False
 
@@ -71,22 +78,28 @@ if __name__ == "__main__":
 
     hash_checksum(sys.argv[1])
     file.read(446)   # Skip executable code
+    
+    partition_start_addrs = []
+    FAT_types = []
+    #Parsing MBR
     while True:
         testEnd = file.read(2)
         if testEnd.hex().upper() == '55AA':
             break
         file.read(2)     # Skip bytes 0-3
         byte = file.read(1).hex().upper()  # Byte 4, Type of Partition
-        
+
         file.read(3)     # Skip bytes 5-7
 
-        partition_start_addrs = []
 
         part_start = ""
         for i in range(4):
             part_start = file.read(1).hex() + part_start
-        partition_start_addrs.append(part_start)
-
+        
+        if byte in ('01', '04', '06', '0B', '0C', '1B', '86'):      #Check if FAT type FS  
+            partition_start_addrs.append(part_start)
+            FAT_types.append(byte)
+            
         part_size = ""
         for i in range(4):
             part_size = file.read(1).hex() + part_size
@@ -94,4 +107,68 @@ if __name__ == "__main__":
         mbr_output = '(' + byte + ') ' + partitionTypes[byte] + ', ' + str(getInt(part_start, 20)).zfill(10)+ ', ' + str(getInt(part_size, 20)).zfill(10)
 
         print(mbr_output)
+    print("\n\n")
+    #Parsing VBR    
+    #print (partition_start_addrs)
+    
+    offset = 0 #Need to include offset as each partition does not take into account where file reader is
+    for i in range(len(partition_start_addrs)):
+        skip_bytes = (getInt(partition_start_addrs[i], 20) - 1) * 512 - offset
+        #print ("SKIPPING BYTES: " + str(skip_bytes))
+        file.read(skip_bytes)
+        offset += skip_bytes
+        print("Partition ", i, ": ", partitionTypes[FAT_types[i]])
+        #Sectors Per Cluster
+        file.read(13)
+        byte = file.read(1)
+        sectors_per_cluster = getInt(byte.hex(), 20)
+
+        #Reserved Area
+        byte = file.read(1).hex()
+        byte = file.read(1).hex() + byte
+
+        reserved_sectors = getInt(byte, 20)
+        
+        num_FAT = getInt(file.read(1).hex(), 20)
+        
+        byte = ''
+        #FAT Area
+        if FAT_types[i] in ('01','04','06','86'): #Checking if not 32-bit
+            file.read(5)
+            byte = file.read(1).hex()
+            byte = file.read(1).hex() + byte
+            file.read(16)
+        else:
+            file.read(19)
+            for j in range(4):
+                byte = file.read(1).hex() + byte
+                
+        #print (byte)
+        size_FAT = getInt(byte, 20)
+        total_FAT_sectors= num_FAT * size_FAT
+        
+        offset += 40 #skipping the amount of bytes that have been read in
+        
+        #Cluster 2 of FAT: Including start of partition in calculation
+        first_cluster = getInt(partition_start_addrs[i], 20) +reserved_sectors + total_FAT_sectors
+        
+        vbr_output =    "Reserved Area: Start Sector: 0 Ending Sector: " + str(reserved_sectors-1) + " Size: " + str(reserved_sectors) + " Sectors" + \
+                        "\nSectors Per Cluster: " + str(sectors_per_cluster) + " Sectors" + \
+                        "\nFAT Area: Start Sector " + str(reserved_sectors) + " Ending Sector: " + str(total_FAT_sectors) + \
+                        "\nNumber of FATS: " + str(num_FAT) + \
+                        "\nSize of Each FAT: " + str(size_FAT) + " Sectors" + \
+                        "\nFirst Sector of Cluster Two: Sector " + str(first_cluster) + "\n\n"
+                        
+        
+        print(vbr_output)
+
+        #Starting/Ending Sector
+        #Size
+        #Sectors Per Cluster
+        #FAT Area
+        #Number of FATS
+        #Size of Each FAT in sectors
+        #First Sector of Cluster 2
+        
+    
     # print('exiting')
